e94ac919cb5cefb2aef9f513740074bb69661c03
Renatho <renatho@pop-os.localdomain>
2023-10-05 10:06:58 -0300

    erros corrigidos

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS SpotifyClone;
+-- DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
 CREATE TABLE SpotifyClone.planos(
     plano_id INT PRIMARY KEY AUTO_INCREMENT,
     plano_nome VARCHAR(50) NOT NULL UNIQUE,
-    plano_preco DECIMAL(4,2) NOT NULL,
+    plano_preco DECIMAL(4,2) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.usuarios(
@@ -13,12 +13,12 @@ CREATE TABLE SpotifyClone.usuarios(
     usuario_idade INT NOT NULL,
     plano_id INT NOT NULL,
     FOREIGN KEY (plano_id) REFERENCES SpotifyClone.planos(plano_id),
-    data_assinatura DATE NOT NULL,
+    data_assinatura DATE NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.artistas(
     artista_id INT PRIMARY KEY AUTO_INCREMENT,
-    artista_nome VARCHAR(50) NOT NULL,
+    artista_nome VARCHAR(50) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.album(
@@ -34,7 +34,7 @@ CREATE TABLE SpotifyClone.cancoes(
     album_id INT NOT NULL,
     FOREIGN KEY (album_id) REFERENCES SpotifyClone.album(album_id),
     ano_lancamento INT NOT NULL,
-    duracao_segundos INT NOT NULL,
+    duracao_segundos INT NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.seguidor(
@@ -51,34 +51,34 @@ CREATE TABLE SpotifyClone.historico(
     cancao_id INT NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios(usuario_id),
     FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancoes(cancao_id),
-    data_reproducao DATE NOT NULL,
+    data_reproducao DATE NOT NULL
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.usuarios (usuario_nome, idade, plano_id, data_assinatura)
-  VALUES ('Barbara Liskov', 82, 1, 2019-10-20),
-  ('Robert Cecil Martin', 58, 1, 2017-01-06)
-  ('Ada Lovelace', 37, 2, 2017-12-30),
-  ('Martin Fowler', 46, 2, 2017-01-17),
-  ('Sandi Metz', 58, 2, 2018-04-29),
-  ('Paulo Freire', 19, 3, 2018-02-14),
-  ('Bell Hooks', 26, 3, 2018-01-05),
-  ('Christopher Alexander', 85, 4, 2019-06-05),
-  ('Judith Butler', 45, 4, 2020-05-13),
-  ('Jorge Amado', 58, 4, 2017-02-17);
+INSERT INTO SpotifyClone.planos (plano_id, plano_nome, plano_preco)
+    VALUES (1, 'gratuito', 0),
+    (2, 'familiar', 7.99),
+    (3, 'universitario', 5.99),
+    (4, 'pessoal', 6.99);
 
-INSERT INTO SpotifyClone.planos (plano_nome, plano_preco)
-    VALUES ('gratuito', 0),
-    ('familiar', 7.99),
-    ('universitario', 5.99),
-    ('pessoal', 6.99);
+INSERT INTO SpotifyClone.usuarios (usuario_nome, usuario_idade, plano_id, data_assinatura)
+  VALUES ('Barbara Liskov', 82, 1, '2019-10-20'),
+  ('Robert Cecil Martin', 58, 1, '2017-01-06'),
+  ('Ada Lovelace', 37, 2, '2017-12-30'),
+  ('Martin Fowler', 46, 2, '2017-01-17'),
+  ('Sandi Metz', 58, 2, '2018-04-29'),
+  ('Paulo Freire', 19, 3, '2018-02-14'),
+  ('Bell Hooks', 26, 3, '2018-01-05'),
+  ('Christopher Alexander', 85, 4, '2019-06-05'),
+  ('Judith Butler', 45, 4, '2020-05-13'),
+  ('Jorge Amado', 58, 4, '2017-02-17');
 
-INSERT INTO SpotifyClone.artistas (artista_nome)
-    VALUES ('Beyoncé'),
-    ('Queen'),
-    ('Elis Regina'),
-    ('Baco Exu do Blues'),
-    ('Blind Guardian'),
-    ('Nina Simone');
+INSERT INTO SpotifyClone.artistas (artista_id ,artista_nome)
+    VALUES (1, 'Beyoncé'),
+    (2, 'Queen'),
+    (3, 'Elis Regina'),
+    (4, 'Baco Exu do Blues'),
+    (5, 'Blind Guardian'),
+    (6, 'Nina Simone');
 
 INSERT INTO SpotifyClone.album (album_nome, artista_id)
     VALUES ('Renaissance', 1),
@@ -120,20 +120,21 @@ INSERT INTO SpotifyClone.seguidor (usuario_id, artista_id)
     (10, 2);
 
 INSERT INTO SpotifyClone.historico (usuario_id, cancao_id, data_reproducao)
-    VALUES (1, 8, 2022-02-28),
-    (1, 2, 2020-05-02 ),
-    (1, 10, 2020-03-06 ),
-    (2, 10, 2022-08-05 ),
-    (2, 7, 2020-01-02 ),
-    (3, 10, 2020-11-13 ),
-    (3, 2, 2020-12-05 ),
-    (4, 8, 2021-08-15 ),
-    (5, 8, 2022-01-09 ),
-    (5, 5, 2020-08-06 ),
-    (6, 7, 2017-01-24 ),
-    (6, 1, 2017-10-12 ),
-    (7, 4, 2011-12-15 ),
-    (8, 4, 2012-03-17 ),
-    (9, 9, 2022-02-24 ),
-    (10, 3, 2015-12-13 );
+VALUES 
+    (1, 8, '2022-02-28 10:45:55'),
+    (1, 2, '2020-05-02 05:30:35'),
+    (1, 10, '2020-03-06 11:22:33'),
+    (2, 10, '2022-08-05 08:05:17'),
+    (2, 7, '2020-01-02 07:40:33'),
+    (3, 10, '2020-11-13 16:55:13'),
+    (3, 2, '2020-12-05 18:38:30'),
+    (4, 8, '2021-08-15 17:10:10'),
+    (5, 8, '2022-01-09 01:44:33'),
+    (5, 5, '2020-08-06 15:23:43'),
+    (6, 7, '2017-01-24 00:31:17'),
+    (6, 1, '2017-10-12 12:35:20'),
+    (7, 4, '2011-12-15 22:30:49'),
+    (8, 4, '2012-03-17 14:56:41'),
+    (9, 9, '2022-02-24 21:14:22'),
+    (10, 3, '2015-12-13 08:30:22');
 ```
